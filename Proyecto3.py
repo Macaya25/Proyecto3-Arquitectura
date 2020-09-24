a5c56bb2c99a2ac5db76583df69f7f1125f2f0db
Macaya25 <afmacaya@miuandes.cl>
2020-09-24 20:15:39 -0300

    Estructura basica?.

--- Proyecto3.py
+++ Proyecto3.py
@@ -1,1 +1,11 @@
-print("Texto prueba")
+try:
+    file = open("instrucciones.txt", 'r')
+except IOError:
+    print("No se encontro el archivo")
+    exit()
+
+
+Lineas = file.readlines()
+for linea in Lineas:
+    print(linea.split())
+file.close()
