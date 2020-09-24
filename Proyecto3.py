35f50e29ac4918748915c4116193003b9af19eac
Macaya25 <afmacaya@miuandes.cl>
2020-09-24 20:41:57 -0300

    Revisado las instrucciones en la pos 0.

--- Proyecto3.py
+++ Proyecto3.py
@@ -1,11 +1,15 @@
 try:
-    file = open("instrucciones.txt", 'r')
+    file = open("Incorrecto.txt", 'r')
 except IOError:
     print("No se encontro el archivo")
     exit()
 
 
 Lineas = file.readlines()
+instructions = ["CMP", "JEQ", "MOV", "SUB", "ADD", "JMP"]
 for linea in Lineas:
-    print(linea.split())
+    line = linea.split()
+    if line[0] not in instructions:
+        print("Error en la linea: {} \t{} no es una instruccion valida".format(linea, line[0]))
+
 file.close()
